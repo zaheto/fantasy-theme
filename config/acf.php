419cbdd1ae1f45412f914c5c23d107c3a6c8624c
zaheto <zaheto@gmail.com>
2024-05-23 20:29:27 +0300

    init theme

---
+++ config/acf.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Field Type Settings
+    |--------------------------------------------------------------------------
+    |
+    | Here you can set default field group and field type configuration that
+    | is then merged with your field groups when they are composed.
+    |
+    | This allows you to avoid the repetitive process of setting common field
+    | configuration such as `ui` on every `trueFalse` field or your
+    | preferred `instruction_placement` on every `fieldGroup`.
+    |
+    */
+
+    'defaults' => [
+        // 'trueFalse' => ['ui' => 1],
+        // 'select' => ['ui' => 1],
+    ],
+
+    // 'defaults' => [
+    //     'fieldGroup' => ['instruction_placement' => 'acfe_instructions_tooltip'],
+    //     'repeater' => ['layout' => 'block', 'acfe_repeater_stylised_button' => 1],
+    //     'trueFalse' => ['ui' => 1],
+    //     'select' => ['ui' => 1],
+    //     'postObject' => ['ui' => 1, 'return_format' => 'object'],
+    //     'accordion' => ['multi_expand' => 1],
+    //     'group' => ['layout' => 'table', 'acfe_group_modal' => 1],
+    //     'tab' => ['placement' => 'left'],
+    //     'sidebar_selector' => ['default_value' => 'sidebar-primary', 'allow_null' => 1]
+    // ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Field Types
+    |--------------------------------------------------------------------------
+    |
+    | Here you can define custom field types that are not included with ACF
+    | out of the box. This allows you to use the fluent builder pattern with
+    | custom field types such as `addEditorPalette()`.
+    |
+    */
+
+    'types' => [
+        // 'editorPalette' => 'editor_palette',
+        // 'phoneNumber' => 'phone_number',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Manifest Path
+    |--------------------------------------------------------------------------
+    |
+    | Here you can define the cache manifest path. Fields are typically cached
+    | when running the `acf:cache` command. This will cache the built field
+    | groups and potentially improve performance in complex applications.
+    |
+    */
+
+    'manifest' => storage_path('framework/cache'),
+
+];
---
+++ resources/views/sections/header.blade.php
@@ -0,0 +1,13 @@
+<header class="banner">
+  <a class="brand" href="{{ home_url('/') }}">
+    {!! $siteName !!}
+  </a>
+
+  @if (has_nav_menu('primary_navigation'))
+    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+    </nav>
+  @endif
+</header>
+
+<x-iconsax-bol-airdrop class="w-6 h-6 text-gray-500" />
